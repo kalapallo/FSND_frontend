8b114c19d6b6837f323c1776302fbb9b11b6af4b
Hannu <basmati@rice.com>
2018-08-19 15:17:12 +0800

    Add missing code from last commit

--- js/app.js
+++ js/app.js
@@ -1,29 +1,37 @@
-var list = [
+var locations = [
     {
         name: "Geylang Serai Market",
-        id: "ChIJ9YySoRAY2jERp62zayDnKuM"
+        id: "ChIJ9YySoRAY2jERp62zayDnKuM",
+        location: {lat: 1.316728, lng: 103.898277}
     },
     {
         name: "Long Phung",
-        id: "ChIJqTY_gxEY2jERYmK8LIyivgo"
+        id: "ChIJqTY_gxEY2jERYmK8LIyivgo",
+        location: {lat: 1.312713, lng: 103.900143}
     },
     {
         name: "Little Vietnam",
-        id: "ChIJPbH-KxYY2jERWtR5TBtZ-DU"
+        id: "ChIJPbH-KxYY2jERWtR5TBtZ-DU",
+        location: {lat: 1.314085, lng: 103.891805}
     },
     {
-        name: "Carpmael Park",
-        id: "ChIJWwnRwxMY2jERYUe9ex2yBMw"
+        name: "Fei Fei Wanton Noodle",
+        id: "ChIJbf1bwhEY2jER4T3aaybaPcM",
+        location: {lat: 1.313522, lng: 103.90227}
     },
     {
-        name: "Tanjong Katong Complex",
-        id: "ChIJB5iUeRcY2jERTV7fJij_i4A"
+        name: "VeganBurg Singapore",
+        id: "ChIJTY2khwQY2jERlMZdzuX1VfA",
+        location: {lat: 1.320948, lng: 103.905471}
     }
-]
+];
+
+var markers = [];
 
 var Location = function(data) {
     this.name = ko.observable(data.name);
     this.id = ko.observable(data.id);
+    this.display = ko.observable(true);
 };
 
 var ViewModel = function() {
@@ -31,14 +39,42 @@ var ViewModel = function() {
 
     this.locationList = ko.observableArray([]);
 
-    list.forEach(function(location) {
+    locations.forEach(function(location) {
         self.locationList.push(new Location(location));
-        //console.log("added: " + location.name);
     })
 
-    // NOTE: clicking on items must be handled by knockout
     this.selectLocation = function(location) {
-        alert("selected " + "'" + location.name() + "'");
+        // Find marker based on location and trigger click event
+        for (var marker of markers) {
+            if (marker.title == location.name()) {
+                google.maps.event.trigger(marker, 'click');
+            }
+        }
+    };
+
+    this.filterLocations = function(data, event) {
+        var value = event.target.value;
+
+        //console.log(value);
+        //console.log(self.locationList());
+
+        self.locationList().forEach(function(location) {
+            if (location.name().includes(value)) {
+                //console.log("found! " + location.name());
+                location.display(true);
+            }
+            else {
+                location.display(false);
+            }
+        })
+
+        // Search for value in location list
+        //for (var location of locations) {
+        //    if (location.name.includes(value)) {
+                //candidates.push(location);
+                //self.locationList().push
+        //    }
+        //}
     };
 };
 
